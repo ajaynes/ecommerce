7ba28f7180edbde1e6a0664589b124098156c0b6
Ashley Jaynes <ashley.jaynes@mac.com>
2023-12-05 10:01:48 -0800

    added confirmation for removing item from cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -6,11 +6,29 @@ export default function Cart() {
     const dispatch = useDispatch();
     const cart = useSelector((state) => state.cart);
 
+    const confirmationDeletion = (product) => {
+        if (confirm('Are you sure?')) {
+            dispatch(deleteCartItem(product.id))
+        }
+    }
+
+    const confirmationDecrement = (product) => {
+        if (product.quantity === 1) {
+            confirmationDeletion(product)
+        } else {
+            dispatch(decrementCart(product))
+        }
+    }
+
     return (
         <>
         <h1>Cart Page</h1>
         <div>{cart.map(product => (
-            <div>title: {product.title}, qty: {product.quantity}<button onClick={() => dispatch(incrementCart(product))}>Add</button><button onClick={() => dispatch(decrementCart(product))}>Subtract</button><button onClick={() => dispatch(deleteCartItem(product.id))}>X</button></div>
+            <div>title: {product.title}, qty: {product.quantity}
+            <button onClick={() => dispatch(incrementCart(product))}>Add</button>
+            <button onClick={() => confirmationDecrement(product)}>Subtract</button>
+            <button onClick={() => confirmationDeletion(product)}>X</button>
+            </div>
         ))}</div>
         </>
     )
