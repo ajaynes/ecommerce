b178606cd508799e9c485b4d3b19b284a27b9eaa
Ashley Jaynes <ashley.jaynes@mac.com>
2024-01-03 18:30:23 -0800

    removed the sidebar filter component because it was causing unecessary prop drilling

--- src/components/CategorySidebar.js
+++ src/components/CategorySidebar.js
@@ -1,14 +1,24 @@
 import PropTypes from "prop-types";
-import ProductFilters from "./ProductFilters";
 export default function CategorySidebar({ filter, products, clearFilter }) {
+
+  const ids = products.map(({ brand }) => brand);
+  const filtered = products.filter(
+    ({ brand }, index) => !ids.includes(brand, index + 1),
+  );
   return (
     <>
       <div>Sidebar</div>
-      <ProductFilters
-        filter={filter}
-        products={products}
-        clearFilter={clearFilter}
-      />
+      {filtered.map((item) => (
+        <button
+          key={item.id}
+          data-type="brand"
+          value={item.brand}
+          onClick={filter}
+        >
+          {item.brand}
+        </button>
+      ))}
+      <button onClick={clearFilter}>Clear</button>
     </>
   );
 }
